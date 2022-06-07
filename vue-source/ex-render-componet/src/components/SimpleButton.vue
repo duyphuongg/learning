e4f2df054cb25c76094a4172d07002d9842da987
phuongbd <phuongbd@fireapps.vn>
2022-06-07 18:39:11 +0700

    clear src

--- vue-source/ex-render-componet/src/components/SimpleButton.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <div>
-        <button>Simple button</button>
-    </div>
-</template>
--- vue-source/ex-render-componet/src/components/SimpleCard.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <div>
-        <p>Async loaded card</p>
-    </div>
-</template>
